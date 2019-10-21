642d9d9505f012f9425ca31a9fd6bcbf7da276e6
amanarya14 <56831138+amanarya14@users.noreply.github.com>
2019-10-21 18:52:37 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,10 @@
+#Link- https://www.hackerrank.com/challenges/python-loops/problem
+
+#Solution
+
+if __name__ == '__main__':
+    n = int(input())
+    x=0
+    while x<n :
+        print(x*x)
+        x+=1
