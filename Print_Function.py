48e678153efa303c73179906b27dfc1b4585fc9b
amanarya14 <56831138+amanarya14@users.noreply.github.com>
2019-10-21 18:43:26 +0530

    Add files via upload

---
+++ Print_Function.py
@@ -0,0 +1,11 @@
+#Link: -
+
+#https://www.hackerrank.com/challenges/python-print/problem
+
+
+#Solution: -
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n) :
+        print (i+1,end="")
